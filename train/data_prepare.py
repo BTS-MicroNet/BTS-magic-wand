dc3ef290280ab9c9de1312d59eb8c4bfe9537b4f
meeeejin <meeeeejin@naver.com>
2021-07-11 17:54:10 +0900

    Comment out the code for consonants

--- train/data_prepare.py
+++ train/data_prepare.py
@@ -145,10 +145,10 @@ if __name__ == "__main__":
         prepare_original_data(folder, name, data,
                               "./data/%s/%s/%s.txt" % (folder, size, name))
     # consonant data
-    for idx2, name in enumerate(consonants):
-      for idx3, size in enumerate(sizes):
-        prepare_original_data(folder, name, data,
-                              "./data/%s/%s/%s.txt" % (folder, size, name))
+    #for idx2, name in enumerate(consonants):
+    #  for idx3, size in enumerate(sizes):
+    #    prepare_original_data(folder, name, data,
+    #                          "./data/%s/%s/%s.txt" % (folder, size, name))
   #generate_negative_data(data)
   print("data_length: " + str(len(data)))
   if not os.path.exists("./data"):
