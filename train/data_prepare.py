a6e9a1cec26fb8e4f1d5c453ba45d5248db08812
meeeejin <meeeeejin@naver.com>
2021-07-11 17:41:58 +0900

    Update data_prepare.py to preprocess hangul data

--- train/data_prepare.py
+++ train/data_prepare.py
@@ -17,7 +17,7 @@
 
 """Prepare data for further process.
 
-Read data from "/slope", "/ring", "/wing", "/negative" and save them
+Read data from "/vowel", "/consonant" and save them
 in "/data/complete_data" in python dict format.
 
 It will generate a new file with the following structure:
@@ -36,51 +36,38 @@ import random
 
 LABEL_NAME = "gesture"
 DATA_NAME = "accel_ms2_xyz"
-folders = ["wing", "ring", "slope"]
-names = [
-    "hyw", "shiyun", "tangsy", "dengyl", "zhangxy", "pengxl", "liucx",
-    "jiangyh", "xunkai"
+folders = ["vowel", "consonant"]
+vowels = [
+    "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ",
+    "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ",
+    "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"
+]
+consonants = [
+    "ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ",
+    "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "ㄲ", "ㄸ",
+    "ㅃ", "ㅆ", "ㅉ"
 ]
 
 
 def prepare_original_data(folder, name, data, file_to_read):  # pylint: disable=redefined-outer-name
   """Read collected data from files."""
-  if folder != "negative":
-    with open(file_to_read, "r") as f:
-      lines = csv.reader(f)
-      data_new = {}
-      data_new[LABEL_NAME] = folder
-      data_new[DATA_NAME] = []
-      data_new["name"] = name
-      for idx, line in enumerate(lines):  # pylint: disable=unused-variable,redefined-outer-name
-        if len(line) == 3:
-          if line[2] == "-" and data_new[DATA_NAME]:
-            data.append(data_new)
-            data_new = {}
-            data_new[LABEL_NAME] = folder
-            data_new[DATA_NAME] = []
-            data_new["name"] = name
-          elif line[2] != "-":
-            data_new[DATA_NAME].append([float(i) for i in line[0:3]])
-      data.append(data_new)
-  else:
-    with open(file_to_read, "r") as f:
-      lines = csv.reader(f)
-      data_new = {}
-      data_new[LABEL_NAME] = folder
-      data_new[DATA_NAME] = []
-      data_new["name"] = name
-      for idx, line in enumerate(lines):
-        if len(line) == 3 and line[2] != "-":
-          if len(data_new[DATA_NAME]) == 120:
-            data.append(data_new)
-            data_new = {}
-            data_new[LABEL_NAME] = folder
-            data_new[DATA_NAME] = []
-            data_new["name"] = name
-          else:
-            data_new[DATA_NAME].append([float(i) for i in line[0:3]])
-      data.append(data_new)
+  with open(file_to_read, "r") as f:
+    lines = csv.reader(f)
+    data_new = {}
+    data_new[LABEL_NAME] = folder
+    data_new[DATA_NAME] = []
+    data_new["name"] = name
+    for idx, line in enumerate(lines):  # pylint: disable=unused-variable,redefined-outer-name
+      if len(line) == 3:
+        if line[2] == "-" and data_new[DATA_NAME]:
+          data.append(data_new)
+          data_new = {}
+          data_new[LABEL_NAME] = folder
+          data_new[DATA_NAME] = []
+          data_new["name"] = name
+        elif line[2] != "-":
+          data_new[DATA_NAME].append([float(i) for i in line[0:3]])
+    data.append(data_new)
 
 
 def generate_negative_data(data):  # pylint: disable=redefined-outer-name
@@ -151,13 +138,15 @@ def write_data(data_to_write, path):
 if __name__ == "__main__":
   data = []  # pylint: disable=redefined-outer-name
   for idx1, folder in enumerate(folders):
-    for idx2, name in enumerate(names):
+    # vowel data
+    for idx2, name in enumerate(vowels):
       prepare_original_data(folder, name, data,
-                            "./%s/output_%s_%s.txt" % (folder, folder, name))
-  for idx in range(5):
-    prepare_original_data("negative", "negative%d" % (idx + 1), data,
-                          "./negative/output_negative_%d.txt" % (idx + 1))
-  generate_negative_data(data)
+                            "./%s/%s.txt" % (folder, name))
+    # consonant data
+    for idx2, name in enumerate(consonants):
+      prepare_original_data(folder, name, data,
+                            "./%s/%s.txt" % (folder, name))
+  #generate_negative_data(data)
   print("data_length: " + str(len(data)))
   if not os.path.exists("./data"):
     os.makedirs("./data")
