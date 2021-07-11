16de137aca4b404716ecd3ce98b5d24d76b26ab4
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-11 21:04:42 +0900

    Remove process of loading pre-trained weight

--- train/train.py
+++ train/train.py
@@ -110,7 +110,7 @@ def build_net(args, seq_length):
 
 def train_net(
     model,
-    model_path,  # pylint: disable=unused-argument
+    # model_path,  # pylint: disable=unused-argument
     train_len,  # pylint: disable=unused-argument
     train_data,
     valid_len,
@@ -194,10 +194,29 @@ if __name__ == "__main__":
         load_data("./data/train", "./data/valid", "./data/test", seq_length)
 
   print("Start to build net...")
-  model, model_path = build_net(args, seq_length)
+  # model, model_path = build_net(args, seq_length)
+  model = tf.keras.Sequential([
+      tf.keras.layers.Conv2D(
+          8, (4, 3),
+          padding="same",
+          activation="relu",
+          input_shape=(seq_length, 3, 1)),  # output_shape=(batch, 128, 3, 8)
+      tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
+      tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
+      tf.keras.layers.Conv2D(16, (4, 1), padding="same",
+                             activation="relu"),  # (batch, 42, 1, 16)
+      tf.keras.layers.MaxPool2D((3, 1), padding="same"),  # (batch, 14, 1, 16)
+      tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
+      tf.keras.layers.Flatten(),  # (batch, 224)
+      tf.keras.layers.Dense(16, activation="relu"),  # (batch, 16)
+      tf.keras.layers.Dropout(0.1),  # (batch, 16)
+      tf.keras.layers.Dense(21, activation="softmax")  # (batch, 4)
+  ])
 
   print("Start training...")
-  train_net(model, model_path, train_len, train_data, valid_len, valid_data,
+  # train_net(model, model_path, train_len, train_data, valid_len, valid_data,
+  #           test_len, test_data, args.model)
+  train_net(model, train_len, train_data, valid_len, valid_data,
             test_len, test_data, args.model)
 
   print("Training finished!")
