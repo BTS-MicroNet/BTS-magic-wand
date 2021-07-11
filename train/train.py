1d3481c5aae3980dca74cbcc17425fc79af149ca
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-11 22:05:43 +0900

    Configure train settings

--- train/train.py
+++ train/train.py
@@ -197,18 +197,18 @@ if __name__ == "__main__":
   # model, model_path = build_net(args, seq_length)
   model = tf.keras.Sequential([
       tf.keras.layers.Conv2D(
-          8, (4, 3),
+          42, (4, 3),
           padding="same",
           activation="relu",
           input_shape=(seq_length, 3, 1)),  # output_shape=(batch, 128, 3, 8)
       tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
       tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
-      tf.keras.layers.Conv2D(16, (4, 1), padding="same",
+      tf.keras.layers.Conv2D(84, (21, 1), padding="same",
                              activation="relu"),  # (batch, 42, 1, 16)
       tf.keras.layers.MaxPool2D((3, 1), padding="same"),  # (batch, 14, 1, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
       tf.keras.layers.Flatten(),  # (batch, 224)
-      tf.keras.layers.Dense(16, activation="relu"),  # (batch, 16)
+      tf.keras.layers.Dense(84, activation="relu"),  # (batch, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 16)
       tf.keras.layers.Dense(21, activation="softmax")  # (batch, 4)
   ])
