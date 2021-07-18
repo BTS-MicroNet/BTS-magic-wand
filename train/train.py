944b69c003e9ec2ed1f4abc7ab88a7afee35b0f9
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-19 07:06:05 +0900

    Fix model input/output

--- train/train.py
+++ train/train.py
@@ -215,20 +215,20 @@ if __name__ == "__main__":
   # activation = None,
   # use_bias = True,
   # 35 classes
-  model = tf.keras.Sequential([
+  model1 = tf.keras.Sequential([
       tf.keras.layers.Conv2D(
-          16, (4, 3),
+          4, (4, 3),
           padding="same",
           activation="relu",
-          input_shape=(seq_length, 3, 1)),  # output_shape=(batch, 128, 3, 8)
-      tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
+          input_shape=(180, 3, 1)),  # output_shape=(batch, 128, 3, 8)
+      tf.keras.layers.MaxPool2D((2, 2)),  # (batch, 42, 1, 8)
       tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
-      tf.keras.layers.Conv2D(32, (4, 1), padding="same",
+      tf.keras.layers.Conv2D(8, (4, 1), padding="same",
                              activation="relu"),  # (batch, 42, 1, 16)
       tf.keras.layers.MaxPool2D((3, 1), padding="same"),  # (batch, 14, 1, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
       tf.keras.layers.Flatten(),  # (batch, 224)
-      tf.keras.layers.Dense(32, activation="relu"),  # (batch, 16)
+      tf.keras.layers.Dense(70, activation="relu"),  # (batch, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 16)
       tf.keras.layers.Dense(35, activation="softmax")  # (batch, 4)
   ])
