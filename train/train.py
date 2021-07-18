9400c22e786d6e9f88a1f64289776e438e21ebf6
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-19 06:03:03 +0900

    Fix cnn structure

--- train/train.py
+++ train/train.py
@@ -216,22 +216,38 @@ if __name__ == "__main__":
   # 35 classes
   model = tf.keras.Sequential([
       tf.keras.layers.Conv2D(
-          35, (4, 3),
+          16, (4, 3),
           padding="same",
           activation="relu",
           input_shape=(seq_length, 3, 1)),  # output_shape=(batch, 128, 3, 8)
       tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
       tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
-      tf.keras.layers.Conv2D(70, (4, 1), padding="same",
+      tf.keras.layers.Conv2D(32, (4, 1), padding="same",
                              activation="relu"),  # (batch, 42, 1, 16)
       tf.keras.layers.MaxPool2D((3, 1), padding="same"),  # (batch, 14, 1, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
       tf.keras.layers.Flatten(),  # (batch, 224)
-      tf.keras.layers.Dense(70, activation="relu"),  # (batch, 16)
+      tf.keras.layers.Dense(32, activation="relu"),  # (batch, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 16)
       tf.keras.layers.Dense(35, activation="softmax")  # (batch, 4)
   ])
-
+  # model = tf.keras.Sequential([
+  #     tf.keras.layers.Conv2D(
+  #         8, (4, 3),
+  #         padding="same",
+  #         activation="relu",
+  #         input_shape=(seq_length, 3, 1)),  # output_shape=(batch, 128, 3, 8)
+  #     tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
+  #     tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
+  #     tf.keras.layers.Conv2D(16, (4, 1), padding="same",
+  #                            activation="relu"),  # (batch, 42, 1, 16)
+  #     tf.keras.layers.MaxPool2D((3, 1), padding="same"),  # (batch, 14, 1, 16)
+  #     tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
+  #     tf.keras.layers.Flatten(),  # (batch, 224)
+  #     tf.keras.layers.Dense(16, activation="relu"),  # (batch, 16)
+  #     tf.keras.layers.Dropout(0.1),  # (batch, 16)
+  #     tf.keras.layers.Dense(4, activation="softmax")  # (batch, 4)
+  # ])
 
   print("Start training...")
   print(model.summary())
