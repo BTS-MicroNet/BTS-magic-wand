831ee0cc118ff64b7a13777e75e45a2c4909c984
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-19 08:17:43 +0900

    Test CNN with no dropout

--- train/test_model.py
+++ train/test_model.py
@@ -16,13 +16,13 @@ model = tf.keras.Sequential([
         activation="relu",
         input_shape=(315, 3, 1)),  # output_shape=(batch, 128, 3, 8)
     tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
-    tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
+    # tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
     tf.keras.layers.Conv2D(8, (4, 1), padding="same",
                            activation="relu"),  # (batch, 42, 1, 16)
     tf.keras.layers.MaxPool2D((3, 1), padding="same"),  # (batch, 14, 1, 16)
-    tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
+    # tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
     tf.keras.layers.Flatten(),  # (batch, 224)
-    # tf.keras.layers.Dense(70, activation="relu"),  # (batch, 16)
+    tf.keras.layers.Dense(70, activation="relu"),  # (batch, 16)
     # tf.keras.layers.Dropout(0.1),  # (batch, 16)
     tf.keras.layers.Dense(35, activation="softmax")  # (batch, 4)
 ])
