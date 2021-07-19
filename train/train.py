2dff33d8299a8901d239534441c096f76a9bc8d3
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-20 00:26:17 +0900

    Add dropout layer after FC layer

--- train/train.py
+++ train/train.py
@@ -332,7 +332,7 @@ if __name__ == "__main__":
       tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
       tf.keras.layers.Flatten(),  # (batch, 224)
       tf.keras.layers.Dense(70, activation="relu"),  # (batch, 16)
-      # tf.keras.layers.Dropout(0.1),  # (batch, 16)
+      tf.keras.layers.Dropout(0.1),  # (batch, 16)
       tf.keras.layers.Dense(35, activation="softmax")  # (batch, 4)
   ])
   print("Start training...")
