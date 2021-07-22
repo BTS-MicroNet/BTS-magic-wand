a39fcca72153f5e180bb365417f6c81bf07cf421
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-23 00:22:25 +0900

    Add dense layer in the last

--- train/test_model.py
+++ train/test_model.py
@@ -26,23 +26,25 @@ def calculate_model_size(model):
   #     tf.keras.layers.Dropout(0.1),  # (batch, 16)
   #     tf.keras.layers.Dense(4, activation="softmax")  # (batch, 4)
   # ])
-model1 = tf.keras.Sequential([
-      tf.keras.layers.Conv2D(
-          8, (4, 3),
-          padding="same",
-          activation="relu",
-          input_shape=(140, 3, 1)),  # output_shape=(batch, 128, 3, 8)
-      tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
-      tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
-      tf.keras.layers.Conv2D(35, (5, 1), padding="same",
-                             activation="relu"),  # (batch, 42, 1, 16)
-      tf.keras.layers.MaxPool2D((5, 1), padding="same"),  # (batch, 14, 1, 16)
-      tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
-      tf.keras.layers.Flatten(),  # (batch, 224)
-      # tf.keras.layers.Dense(70, activation="relu"),  # (batch, 16)
-      tf.keras.layers.Dropout(0.1),  # (batch, 16)
-      tf.keras.layers.Dense(35, activation="softmax")  # (batch, 4)
-  ])
 
 
-print(calculate_model_size(model1))
+model6 = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(
+        8, (4, 3),
+        padding="same",
+        activation="relu",
+        input_shape=(140, 3, 1)),  # output_shape=(batch, 128, 3, 8)
+    tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
+    tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
+    tf.keras.layers.Conv2D(35, (5, 1), padding="same",
+                           activation="relu"),  # (batch, 42, 1, 16)
+    tf.keras.layers.MaxPool2D((5, 1), padding="same"),  # (batch, 14, 1, 16)
+    tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
+    tf.keras.layers.Flatten(),  # (batch, 224)
+    tf.keras.layers.Dense(70, activation="relu"),  # (batch, 16)
+    tf.keras.layers.Dropout(0.1),  # (batch, 16)
+    tf.keras.layers.Dense(35, activation="softmax")  # (batch, 4)
+])
+
+
+print(calculate_model_size(model6))
