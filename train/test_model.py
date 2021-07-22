13800b159564fccdecbdb5008ea046123b2c8260
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-07-23 02:01:59 +0900

    MaxPool by (2,3)

--- train/test_model.py
+++ train/test_model.py
@@ -34,14 +34,14 @@ model6 = tf.keras.Sequential([
         padding="same",
         activation="relu",
         input_shape=(140, 3, 1)),  # output_shape=(batch, 128, 3, 8)
-    tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
+    tf.keras.layers.MaxPool2D((2, 3)),  # (batch, 42, 1, 8)
     tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
-    tf.keras.layers.Conv2D(35, (5, 1), padding="same",
+    tf.keras.layers.Conv2D(16, (5, 1), padding="same",
                            activation="relu"),  # (batch, 42, 1, 16)
-    tf.keras.layers.MaxPool2D((5, 1), padding="same"),  # (batch, 14, 1, 16)
+    tf.keras.layers.MaxPool2D((2, 1), padding="same"),  # (batch, 14, 1, 16)
     tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
     tf.keras.layers.Flatten(),  # (batch, 224)
-    tf.keras.layers.Dense(175, activation="relu"),  # (batch, 16)
+    tf.keras.layers.Dense(140, activation="relu"),  # (batch, 16)
     tf.keras.layers.Dropout(0.1),  # (batch, 16)
     tf.keras.layers.Dense(35, activation="softmax")  # (batch, 4)
 ])
